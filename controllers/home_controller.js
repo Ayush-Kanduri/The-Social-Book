de4abf6e4240a01923523349d33206d76a9fcceb
Ayush <ayush.kanduri@gmail.com>
2022-03-03 20:43:51 +0530

    home.ejs Rendered

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,10 @@
 
 //Export the Home Controller's Home() Function to the "/" Route.
 module.exports.home = (request, response) => {
-	return response.end("<h1>Home Page</h1>");
+	// return response.end("<h1>Home Page</h1>");
+	return response.render("home", {
+		title: "Social Book",
+	});
 };
 
 //Export the Home Controller's Welcome() Function to the "/" Route.
