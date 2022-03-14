4d1c5cc073e2fe0ecf140be698a69c2761c0abc4
Ayush <ayush.kanduri@gmail.com>
2022-03-14 12:58:59 +0530

    User Model created

--- routes/home.js
+++ routes/home.js
@@ -9,8 +9,6 @@ const homeController = require("../controllers/home_controller");
 //SYNTAX: router.METHOD("/", homeController.FUNCTION_NAME);
 //Access the Home Controller's Home() Function by '/' route.
 router.get("/", homeController.home);
-//Call Welcome Function from the Home Controller
-homeController.welcome();
 
 //Export the Router.
 module.exports = router;
