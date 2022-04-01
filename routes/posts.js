f21d518ce2c3bfd80bd54bd7d6f5585363edf51b
Ayush <ayush.kanduri@gmail.com>
2022-04-01 09:24:31 +0530

    Hide Posts Section from the Non-Members Public Homepage in 2 Ways

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,16 @@
 const express = require("express");
 //Create a Local Router
 const router = express.Router();
+//Require the Passport Module
+const passport = require("passport");
+//Requires the Passport Local Strategy Module
+const passportLocal = require("../config/passport-local-strategy");
 
 //Require Posts Controller
 const postsController = require("../controllers/posts_controller");
 
 //Access the Posts Controller's create() Function
-router.post("/create", postsController.create);
+router.post("/create", passport.checkAuthentication, postsController.create);
 
 //Export the Router
 module.exports = router;
