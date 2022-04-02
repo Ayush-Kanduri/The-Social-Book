6d9c1c4018fd58734b5abc47e48d34100eb54bc3
Ayush <ayush.kanduri@gmail.com>
2022-04-02 14:45:58 +0530

    Post Destroy Controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,7 @@
 //Require the Post Model Data Structure
 const Post = require("../models/post");
+//Require the Comment Model Data Structure
+const Comment = require("../models/comment");
 
 //Export the Posts Controller's create() Function
 module.exports.create = (req, res) => {
@@ -18,4 +20,25 @@ module.exports.create = (req, res) => {
 			return res.redirect("back");
 		}
 	);
+};
+
+//Export the Posts Controller's destroy() Function
+module.exports.destroy = (req, res) => {
+	Post.findById(req.params.id, (err, post) => {
+		if (err) {
+			console.log("Error in finding the post");
+			return;
+		}
+		//.id means converting the object id into string
+		//If the User who posted the post === the User logged in
+		if (post.user === req.user.id) {
+			//Delete the Post
+			post.remove();
+			Comment.deleteMany({ post: req.params.id }, (err) => {
+				return res.redirect("back");
+			});
+		} else {
+			return res.redirect("back");
+		}
+	});
 };
