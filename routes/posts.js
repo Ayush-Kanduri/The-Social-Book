34f1d82e327f97ec8f5a276d417a23d0a58cfeb2
Ayush <ayush.kanduri@gmail.com>
2022-04-02 14:47:06 +0530

    Post Destroy Route

--- routes/posts.js
+++ routes/posts.js
@@ -12,6 +12,12 @@ const postsController = require("../controllers/posts_controller");
 
 //Access the Posts Controller's create() Function
 router.post("/create", passport.checkAuthentication, postsController.create);
+//Access the Posts Controller's destroy() Function
+router.get(
+	"/delete/:id",
+	passport.checkAuthentication,
+	postsController.destroy
+);
 
 //Export the Router
 module.exports = router;
