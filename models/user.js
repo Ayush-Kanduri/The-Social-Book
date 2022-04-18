409d3f168885fcb886794ecb485cee5fec918449
Ayush <ayush.kanduri@gmail.com>
2022-04-18 06:50:03 +0530

    Setting up Multer

--- models/user.js
+++ models/user.js
@@ -1,6 +1,16 @@
 //Create the same instance of mongoose which is used in the MongoDB configuration inside config
 const mongoose = require("mongoose");
 
+//We are importing Multer in User Models & not in Config, because we are uploading that file specific to that user & we'll have some specific settings.
+//Avatar would be uploaded somewhere else but we are setting Multer for each Model individually.
+
+//Require Multer Module for the File Uploading
+const multer = require("multer");
+//Require Path Module for the Directory
+const path = require("path");
+//The path where the files will be uploaded
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+
 //Create the DB Schema
 const userSchema = new mongoose.Schema(
 	{
