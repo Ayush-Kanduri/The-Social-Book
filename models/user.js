8f9472927e259c708b982ecb172bd04a9ef4b156
Ayush <ayush.kanduri@gmail.com>
2022-04-18 07:22:12 +0530

    Configuring Multer

--- models/user.js
+++ models/user.js
@@ -27,10 +27,45 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
+		avatar: {
+			type: String,
+		},
 	},
 	{ timestamps: true }
 	// Timestamp will create the createdAt and the updatedAt fields
 );
+
+// -------------------------------------------------------------------
+//Exact Path for the Avatar Uploading: will be the Current Directory relative to the Path of the Uploads Folder
+
+//__dirname is directory/path of the user.js in Models
+//We need to be at '..' (Models Folder)
+//Now we go to the uploads folder using AVATAR_PATH
+
+//Setting up the Disk Storage Engine
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, "..", AVATAR_PATH));
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(null, file.fieldname + "-" + uniqueSuffix);
+	},
+});
+
+//This attaches a Disk Storage of the Multer in the Storage Property
+//single() - Uploads a single file only for the fieldname "avatar"
+
+//Attaching the Disk Storage Engine to the Multer
+//Static Function
+userSchema.statics.uploadedAvatar = multer({ storage: storage }).single(
+	"avatar"
+);
+
+//AVATAR_PATH should be available globally in the User Model using this function() & should tell the controller where the path would be.
+//Static Function
+userSchema.statics.avatarPath = AVATAR_PATH;
+// -------------------------------------------------------------------
 
 //Create a Model/Collection to populate the data with the same name for the schema in the DB
 const User = mongoose.model("User", userSchema);
