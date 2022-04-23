d86979f7d5ad9cbd7072ce2ca21a3c6123c11770
Ayush <ayush.kanduri@gmail.com>
2022-04-23 21:11:48 +0530

    Contact Us & About Us Page Created

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,7 +1,15 @@
-//Export the Contact Controller's Contact() Function to the "/contact" Routes.
+//Export the Contact Controller's contact()
 module.exports.contact = (request, response) => {
 	// return response.end("<h1>Contact Page</h1>");
 	return response.render("contact", {
 		title: "Contact Us",
+	});
+};
+
+//Export the Contact Controller's about()
+module.exports.about = (request, response) => {
+	// return response.end("<h1>Contact Page</h1>");
+	return response.render("about", {
+		title: "About Us",
 	});
 };
--- routes/contact.js
+++ routes/contact.js
@@ -9,6 +9,8 @@ const contactController = require("../controllers/contact_controller");
 //SYNTAX: router.METHOD("Contact: Sub-Router", contactController.FUNCTION_NAME);
 //Access the Contact Controller's Contact() Function by '/contact' route.
 router.get("/", contactController.contact);
+//Access the Contact Controller's About() Function by '/contact/about' route.
+router.get("/about", contactController.about);
 
 //Export the Router
 module.exports = router;
