515f9fda81da58a796f5e1839d7753e56481adc2
Ayush <ayush.kanduri@gmail.com>
2022-04-30 15:51:38 +0530

    environment variables

--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,8 +1,12 @@
+const dotenv = require("dotenv").config();
+//{path: "path/filename"}
+
 //Export the Contact Controller's contact().
 module.exports.contact = (request, response) => {
 	// return response.end("<h1>Contact Page</h1>");
 	return response.render("contact", {
 		title: "Contact Us",
+		api_key: process.env.GOOGLE_MAPS_API_KEY,
 	});
 };
 
