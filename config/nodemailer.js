06124104f2369d4696cb86e632ac5167945d4eab
Ayush <ayush.kanduri@gmail.com>
2022-05-02 15:32:41 +0530

    Setting Up Nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,44 @@
+//Require the Nodemailer Module
+const nodemailer = require("nodemailer");
+//Require the EJS Module
+const ejs = require("ejs");
+//Require the Path Module
+const path = require("path");
+
+//Create a SMTP Transporter
+let transporter = nodemailer.createTransport({
+	service: "gmail",
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false, //For 2FA
+	auth: {
+		//To establish our Identity
+		user: "",
+		pass: "",
+	},
+});
+
+//To send the HTML Template Email
+let renderTemplate = (data, relativePath) => {
+	let mailHTML;
+	ejs.renderFile(
+		//Path to the EJS Template
+		path.join(__dirname, "../views/mailers", relativePath),
+		data,
+		function (err, template) {
+			if (err) {
+				console.log("Error in rendering the template");
+				return;
+			}
+			mailHTML = template;
+		}
+		//Data (Context) to be passed to the EJS Template
+		//Relative Path is the place from where this function is being called.
+	);
+	return mailHTML;
+};
+
+module.exports = {
+	transporter,
+	renderTemplate,
+};
