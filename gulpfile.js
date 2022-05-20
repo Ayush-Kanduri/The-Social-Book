a045103d8e885daf23ad7387731980142732c319
Ayush <ayush.kanduri@gmail.com>
2022-05-20 16:34:17 +0530

    Setting Up Gulp + Using Gulp Minification of SCSS to CSS

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+//Require the Gulp Module for the Gulp Task Runner
+const gulp = require("gulp");
+
+//Require the Gulp-Sass Module for converting the SASS to CSS
+const sass = require("gulp-sass")(require("node-sass"));
+//Require the Gulp-CSSNano Module for Minifying the CSS
+const cssnano = require("gulp-cssnano");
+//Require the Gulp-Rev Module for Renaming the CSS Files
+const rev = require("gulp-rev");
+
+gulp.task("css", (done) => {
+	console.log("Minifying CSS...");
+	//** means any folder and sub-folders inside
+	//**/!(*.min.css) means any file that does not have the .min.css extension
+	//* means any file
+	gulp
+		//Finds all the SASS files in the assets/scss folder
+		.src("./assets/sass/**/*.scss")
+		//The File will pass through the Gulp SASS Module and convert it to CSS
+		.pipe(sass())
+		//The File will pass through the Gulp CSSNano Module and minify it
+		.pipe(cssnano())
+		//Stores the file in the assets/css folder
+		.pipe(gulp.dest("./assets.css"));
+	//Pipe is a function which is calling all these Sub-Middlewares in Gulp
+
+	console.log("Minified CSS...");
+	//Finds the CSS files in the assets/css folder
+
+	gulp
+		.src("./assets/**/*.css")
+		//The File will pass through the Gulp Rev Module and rename it
+		.pipe(rev())
+		//Stores the file in the /public/assets/css folder
+		.pipe(gulp.dest("./public/assets"))
+		//Creates & Stores a Manifest File
+		.pipe(
+			rev.manifest({
+				//Current Working Directory is public as we are taking everything from the public folder
+				cwd: "public",
+				//If the name already exists, it will not change it, but will merge it with the original files
+				merge: true,
+			})
+		)
+		//Stores the Manifest File in the /public/assets folder
+		.pipe(gulp.dest("./public/assets"));
+
+	done();
+});
