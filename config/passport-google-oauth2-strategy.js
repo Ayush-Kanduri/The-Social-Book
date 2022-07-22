8662dd093eff14c6e024c7093d5abc18c2f0b03c
Ayush <ayush.kanduri@gmail.com>
2022-07-22 21:03:58 +0530

    rectified broken images

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -47,7 +47,7 @@ passport.use(
 							email: profile.emails[0].value,
 							password: crypto.randomBytes(20).toString("hex"),
 							avatar: !!profile.photos[0].value
-								? profile.photos[0].value
+								? `${profile.photos[0].value}?`
 								: "",
 						},
 						(err, user) => {
