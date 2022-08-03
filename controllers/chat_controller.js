6c5f4e55c59d9367f8ff1d31720db9c27ab5e9cf
Ayush <ayush.kanduri@gmail.com>
2022-08-03 19:31:44 +0530

    make it better

--- controllers/chat_controller.js
+++ controllers/chat_controller.js
@@ -182,3 +182,69 @@ module.exports.chatRoom = async function (req, res) {
 		});
 	}
 };
+
+module.exports.chatting = async function (req, res) {
+	try {
+		let info = {};
+		let { chat_room, timestamp, message } = req.body;
+		let { friend_email, user_email } = req.body;
+		let alignment = "";
+
+		const user = await User.findOne({ email: user_email }).populate();
+		const friend = await User.findOne({ email: friend_email }).populate();
+
+		let chat = await Chat.create({
+			content: message,
+			sender: user._id,
+			receiver: friend._id,
+			time: timestamp,
+			room: chat_room,
+		});
+
+		let room = await Room.findOne({
+			sender: user._id,
+			receiver: friend._id,
+		});
+
+		if (!room) {
+			room = await Room.findOne({
+				sender: friend._id,
+				receiver: user._id,
+			});
+		}
+
+		await chat.save();
+		room.chats.push(chat);
+		await room.save();
+		user.chats.push(chat);
+		friend.chats.push(chat);
+		await user.save();
+		await friend.save();
+
+		let sender = await User.findById(chat.sender).populate();
+		sender = sender.email;
+		info = {
+			message: message,
+			user_name: user.name,
+			user_email: user.email,
+			friend_name: friend.name,
+			friend_email: friend.email,
+			alignment: alignment,
+			sender: sender,
+			timestamp: timestamp,
+			chat_room: chat_room,
+		};
+		return res.status(200).json({
+			message: "Chat Sent Successfully",
+			data: {
+				info: info,
+			},
+		});
+	} catch (err) {
+		console.log("Error: ", err);
+		return res.status(500).json({
+			message: "Internal Server Error",
+			error: err,
+		});
+	}
+};
